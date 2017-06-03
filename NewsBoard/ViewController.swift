7bb13abe4c35f530da9adc6b047621dadaa531e6
DineshKumar <edineshkumarr@gmail.com>
2017-06-03 09:55:38 +0530

    Initial Commit

---
+++ NewsBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsBoard
+//
+//  Created by DineshKumar on 03/06/17.
+//  Copyright © 2017 DineshKumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
